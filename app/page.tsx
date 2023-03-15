9d27eedc3e10e25d1dd3976a6a2bdbdbcaacd53f
Matt Carroll <mattca@fb.com>
2023-03-15 11:28:51 -0700

    trying oembed

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,69 @@
-type User = {
-  id: string;
-  username: string;
+type TweetEmbed = {
+  url: string;
+  title: string;
+  html: string;
+  width: string;
+  height: string;
+  type: string;
+  cache_age: string;
+  provider_name: string;
+  provider_url: string;
+  version: string;
 };
 
 export default async function Home() {
-  const user: User = await getUser();
+  const EmbeddedTweet = await TwitterEmbedExample();
+  // const EmbeddedInsta = await InstagramEmbedExample();
   return (
     <>
-      <p>🦉2343</p>
-      <p>{user.id}</p>
-      <p>{user.username}</p>
-      <p>🦉2343</p>
+      {EmbeddedTweet}
+      {/* {EmbeddedInsta} */}
     </>
   );
 }
 
-async function getUser() {
-  const url = `https://graph.instagram.com/v16.0/me?fields=id,username,media&access_token=${process.env.INSTAGRAM_TOKEN}`;
-  const response = await fetch(url);
-  const user = await response.json();
-  console.log(user);
-  return user;
+async function TwitterEmbedExample() {
+  const tweet = await getTwitterEmbed(
+    "https://twitter.com/mattcarrollcode/status/1629262255744503809"
+  );
+  return (
+    <>
+      <p>Twitter Embed example:</p>
+      <p>{tweet.url}</p>
+      <p>{tweet.title}</p>
+      <p>{tweet.html}</p>
+      <div dangerouslySetInnerHTML={{ __html: tweet.html }} />
+      <p>{tweet.html}</p>
+      <p>{tweet.width}</p>
+      <p>{tweet.height}</p>
+      <p>{tweet.type}</p>
+      <p>{tweet.cache_age}</p>
+      <p>{tweet.provider_name}</p>
+      <p>{tweet.provider_url}</p>
+      <p>{tweet.version}</p>
+    </>
+  );
+}
+
+async function getTwitterEmbed(url: string) {
+  const oembedURL = `https://publish.twitter.com/oembed?url=${url}`;
+  const response = await fetch(oembedURL);
+  const embed = await response.json();
+  console.log(embed);
+  return embed;
+}
+
+async function InstagramEmbedExample() {
+  const post = await getFacebookEmbed(
+    "https://www.instagram.com/p/CjldKGeBi2K/"
+  );
+  return;
+}
+
+async function getFacebookEmbed(url: string) {
+  const oembedURL = `https://graph.facebook.com/v16.0/instagram_oembed?url=${url}`;
+  const response = await fetch(oembedURL);
+  const embed = await response.json();
+  console.log(embed);
+  return embed;
 }
