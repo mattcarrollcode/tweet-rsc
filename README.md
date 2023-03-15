9870a17eddaf341fca6d848a7e675440185be6da
Matt Carroll <mattca@fb.com>
2023-03-14 17:29:18 -0700

    working with instgram basic API

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Instagram React
 
 This is a React component library for embedding Instagram posts on a React website. It uses React server components. This keeps the fetching and API keys on the sever and prevents them from being exposed on a web page directly.
+
+# Setup
+
+1. create an app
+   1. consumer
+   1. enter basic info
+1. add Instagram basic display product
+1. got to settings > add platforms > enter your website url
+1. Enter app ID and app secret
+1. enter localhost:3000 for url
+1. add Instagram tests and enter your username
+1. edit profile > accept test invite
+1. generate token
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <p>🦉</p>;
+type User = {
+  id: string;
+  username: string;
+};
+
+export default async function Home() {
+  const user: User = await getUser();
+  return (
+    <>
+      <p>🦉2343</p>
+      <p>{user.id}</p>
+      <p>{user.username}</p>
+      <p>🦉2343</p>
+    </>
+  );
+}
+
+async function getUser() {
+  const url = `https://graph.instagram.com/v16.0/me?fields=id,username,media&access_token=${process.env.INSTAGRAM_TOKEN}`;
+  const response = await fetch(url);
+  const user = await response.json();
+  console.log(user);
+  return user;
 }
